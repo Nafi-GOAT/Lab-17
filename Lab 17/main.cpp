aea1f2d559824123284b4588ed024b3a449d4316
Nafi-GOAT <nafih2989@gmail.com>
2025-10-01 16:44:21 -0700

    NEww

--- Lab 17/main.cpp
+++ Lab 17/main.cpp
@@ -9,5 +9,12 @@ using namespace std;
 
 struct Node{
     float value;
+    Node *next;
 };
 
+void output(Node *hd);
+void addFront(Node* head, float val);
+void addTail(Node* head, float val);
+void deleteNode(Node *head, int pos);
+void insertNode(Node* head, int pos,float val);
+void deleteList(Node* head);

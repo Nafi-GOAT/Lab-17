93cb2e592eec6e1bd08612e017d47fed02c798a9
Nafi-GOAT <nafih2989@gmail.com>
2025-10-01 17:14:43 -0700

    new

--- Lab 17/main.cpp
+++ Lab 17/main.cpp
@@ -51,5 +51,15 @@ void addTail(Node* head, float val){
     newNode ->value = val;
     newNode->next = nullptr;
     
+    if  (!head){
+        head = newNode;
+        return;
+        
+    }
     
+    Node* temp = head;
+    while (temp ->next)
+        temp = temp ->next;
+    temp ->next = newNode;
 }
+

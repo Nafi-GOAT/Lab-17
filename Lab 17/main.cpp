b093d86420adaa4681c63e9a2c375fd5d3cd9d76
Nafi-GOAT <nafih2989@gmail.com>
2025-10-02 23:59:01 -0700

    Final

--- Lab 17/main.cpp
+++ Lab 17/main.cpp
@@ -5,166 +5,102 @@
 //  Created by Nafi on 10/1/25.
 //
 #include <iostream>
+#include <cstdlib>   // for rand()
 using namespace std;
 
-struct Node{
+const int SIZE = 7;
+
+// Node definition
+struct Node {
     float value;
     Node *next;
 };
 
-void output(Node *head);
+// Function prototypes
+void output(Node* head);
 void addFront(Node*& head, float val);
 void addTail(Node*& head, float val);
 void deleteNode(Node*& head, int pos);
-void insertNode(Node*& head, int pos,float val);
+void insertNode(Node*& head, int pos, float val);
 void deleteList(Node*& head);
 
-int main(){
+int main() {
     Node *head = nullptr;
-    
-    for (int =0; i< SIZE; i++){
-        int tmp_val = rand () % 100;
-        addFront(head, tmp_val);
+
+    // Create a linked list of size SIZE with random numbers 0-99
+    for (int i = 0; i < SIZE; i++) {
+        int tmp_val = rand() % 100;
+        addFront(head, tmp_val);   // Instead of inline creation, use function
     }
     output(head);
-    
-    cout << "WHICH node to delete? " << endl;
+
+    // Deleting a node
+    cout << "Which node to delete? " << endl;
     output(head);
     int entry;
-    cout << "Choice __> ";
-    cin >>entry;
-    
-    deleteNode(head, entry);
+    cout << "Choice --> ";
+    cin >> entry;
+
+    deleteNode(head, entry);   // Replaced inline deletion with function
     output(head);
-    
-    cout << "After which node to insert 1000?" << endl;
+
+    // Inserting a node
+    cout << "After which node to insert 10000? " << endl;
     output(head);
-    cout <<"Choice--> ";
-    cin>> entry;
-    
-    insertNode(head, entry, 1000);
+    cout << "Choice --> ";
+    cin >> entry;
+
+    insertNode(head, entry, 10000);   // Replaced inline insert with function
     output(head);
-    
-    deleteList(head);
+
+    // Deleting the entire linked list
+    deleteList(head);   // Replaced inline while loop with function
     output(head);
-    
+
     return 0;
 }
-void addFront(Node*& head, float val){
-    Node* newNode = new Node;
-    newNode ->value = val;
-    newNode-> next = head;
-    head = newNode;
-}
+    void insertNode(Node*& head, int pos, float val) {
+        Node* newNode = new Node;
+        newNode->value = val;
+        newNode->next = nullptr;
 
-void addFront (Node*& head, floar val){
-    Node* new
-    addFront(head, 10);
-    addFront(head, 20);
-    addTail(head, 99);
-    output(head);
-    
-    deleteNode(head, 2);
-    output(head);
-    
-    insertNode(head, 1, 1000);
-    output(head);
-    
-    deleteList(head);
-    output(head);
-    
-    return 0;
-}
+        if (pos <= 1 || !head) {
+            newNode->next = head;
+            head = newNode;
+            return;
+        }
 
-void addFront(Node*& head, float val){
-    Node* newNode = new Node;
-    newNode ->value = val;
-    newNode->next = head;
-    head = newNode;
-}
+        Node* current = head;
+        Node* prev = nullptr;
+        for (int i = 1; i < pos && current; i++) {
+            prev = current;
+            current = current->next;
+        }
 
-void addTail(Node*& head, float val){
-    Node* newNode = new Node;
-    newNode->value = val;
-    newNode->next = nullptr;
-    
-    if (!head){
-        head = newNode;
-        return;
-    }
-    
-    Node* temp = head;
-    while (temp ->next)
-        temp = temp->next;
-    temp->next = newNode;
-}
-
-void deleteNode(Node*& head, int pos) {
-    if (!head || pos < 1) return;
-
-    Node* current = head;
-
-    if (pos == 1) {
-        head = head->next;
-        delete current;
-        return;
+        newNode->next = current;
+        if (prev) prev->next = newNode;
     }
 
-    Node* prev = nullptr;
-    for (int i = 1; i < pos && current; i++) {
-        prev = current;
-        current = current->next;
+    void deleteList(Node*& head) {
+        Node* current = head;
+        while (current) {
+            Node* tmp = current->next;
+            delete current;
+            current = tmp;
+        }
+        head = nullptr;
     }
 
-    if (prev && current) {
-        prev->next = current->next;
-        delete current;
+    void output(Node* hd) {
+        if (!hd) {
+            cout << "Empty list.\n";
+            return;
+        }
+        int count = 1;
+        Node* current = hd;
+        while (current) {
+            cout << "[" << count++ << "] " << current->value << endl;
+            current = current->next;
+        }
+        cout << endl;
     }
-}
-
-void insertNode(Node*& head, int pos, float val){
-    Node* newNode = new Node;
-    newNode->value = val;
-    newNode->next = nullptr;
-    
-    if (pos <= 1 || !head){
-        newNode->next = head;
-        head = newNode;
-        return;
-    }
-    
-    Node* current = head;
-    Node* prev = nullptr;
-    
-    for (int i = 1; i<pos && current; i++){
-        prev = current;
-        current = current->next;
-    }
-    
-    newNode ->next = current;
-    if (prev) prev->next = newNode;
-}
- 
-void deleteList(Node*& head){
-    Node* current = head;
-    while (current) {
-        Node* tmp = current ->next;
-        delete current;
-        current = tmp;
-    }
-    head = nullptr;
-}
-
-void output(Node *hd){
-    if(!hd){
-        cout << "Empty list.\n";
-        return;
-    }
-    int count = 1;
-    Node * current = hd;
-    while(current){
-        cout << "["<< count++ << "]" << current->value << endl;
-        current = current->next;
-    }
-    cout << endl;
-}
